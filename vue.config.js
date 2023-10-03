309e499458d9f6689c3847ff297af2477d86b951
Idan Gal <idangal2426@gmail.com>
2023-10-03 22:24:19 +0300

    b

---
+++ vue.config.js
@@ -0,0 +1,22 @@
+// module.exports = {
+//   configureWebpack: {
+//     devtool: "source-map"
+//   },
+//   devServer: {
+//     // host: "132.72.65.211"
+//     host: "localhost"
+//   }
+//   // publicPath: process.env.NODE_ENV === "production" ? "/LAB12/" : "/"
+// };
+
+// // vue.config.js file to be placed in the root of your repository
+
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/Mediphone/'
+//     : '/'
+// }
+
+module.exports = {
+  publicPath: '/Mediphone/'
+}
