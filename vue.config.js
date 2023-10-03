c8625e1ee0a9acc42e2aa52f46799819a1ba5c6b
Idan Gal <idangal2426@gmail.com>
2023-10-03 22:13:00 +0300

    x

--- vue.config.js
+++ vue.config.js
@@ -11,8 +11,12 @@
 
 // // vue.config.js file to be placed in the root of your repository
 
+// module.exports = {
+//   publicPath: process.env.NODE_ENV === 'production'
+//     ? '/Mediphone/'
+//     : '/'
+// }
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Mediphone/'
-    : '/'
+  publicPath: '/Mediphone/'
 }
